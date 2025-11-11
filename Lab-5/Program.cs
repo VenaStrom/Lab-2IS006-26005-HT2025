935d840e62c9f5eaa1f0dcc77ca1e2c9fe1cc73e
Vena <strom.vena+gh@gmail.com>
2025-11-11 16:37:08 +0100

    do lab 5

---
+++ Lab-5/Program.cs
@@ -0,0 +1,42 @@
+﻿internal class Program
+{
+  private static void Main(string[] args)
+  {
+    Rectangle rect = new("43 23");
+    rect.Print();
+  }
+}
+class Rectangle
+{
+  public Rectangle(double height, double width)
+  {
+    this.height = height;
+    this.width = width;
+  }
+  public Rectangle(double size) : this(size, size) { } // Square
+  public Rectangle() : this(1.0, 1.0) { } // Default to unit square
+  public Rectangle(Rectangle other) : this(other.height, other.width) { } // Copy constructor
+  public Rectangle(string dimensions)
+  {
+    char[] separators = ['x', 'X', ' ', ','];
+    string[] parts = dimensions.Split(separators, StringSplitOptions.RemoveEmptyEntries);
+
+    if (parts.Length == 2 &&
+      double.TryParse(parts[0], out double h) &&
+      double.TryParse(parts[1], out double w))
+    {
+      height = h;
+      width = w;
+    }
+    else
+    {
+      throw new ArgumentException("Invalid dimensions format. Use 'height x width'.");
+    }
+  }
+
+  private double width;
+  private double height;
+
+  public void Print() =>
+    Console.WriteLine($"({height} x {width})");
+}
