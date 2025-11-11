399ef3cdb18e68a8da31b36fcc34968fd08bc118
Vena <strom.vena+gh@gmail.com>
2025-11-11 17:01:31 +0100

    start lab 6

---
+++ Lab-6/Program.cs
@@ -0,0 +1,46 @@
+﻿internal class Program
+{
+  private static void Main(string[] args)
+  {
+    Car myCar = new();
+    myCar.Speed = 50;
+    Console.WriteLine("Initial Speed: " + myCar.Speed);
+
+    myCar.Accelerate(30);
+    Console.WriteLine("After Accelerating by 30: " + myCar.Speed);
+
+    myCar.Brake(60);
+    Console.WriteLine("After Braking by 60: " + myCar.Speed);
+
+    myCar.Accelerate(250);
+    Console.WriteLine("After Accelerating by 250: " + myCar.Speed);
+  }
+}
+
+class Car
+{
+  private double speed;
+  private double topSpeed = 200;
+
+  public double Speed
+  {
+    get { return speed; }
+    set { speed = value; }
+  }
+
+  public void Accelerate(double amount)
+  {
+    double newSpeed = speed + amount;
+    if (newSpeed < 0) speed = 0;
+    else if (newSpeed > topSpeed) speed = topSpeed;
+    else speed = newSpeed;
+  }
+
+  public void Brake(double amount)
+  {
+    double newSpeed = speed - amount;
+    if (newSpeed < 0) speed = 0;
+    else if (newSpeed > topSpeed) speed = topSpeed;
+    else speed = newSpeed;
+  }
+}
